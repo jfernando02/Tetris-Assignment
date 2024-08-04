767c08d8bb53223a95a3b90348cc683f463c5f8b
Stefan Barosan <stefanmb@hotmail.com>
2024-08-04 10:25:37 +1000

    Fixed some somments

--- TetrisGame/src/ui/MainFrame.java
+++ TetrisGame/src/ui/MainFrame.java
@@ -1,4 +1,4 @@
-// Main application window for the game (after the SplashScreen)
+// Main application window for the game (Holds Splash Screen and the Main Panel
 // Uses the UIGenerator class to generate the UI components via the panels
 package ui;
 

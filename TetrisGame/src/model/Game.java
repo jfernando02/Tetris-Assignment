48ab4277579a9fadf1bbeb96f49a047a70706a26
jfernando02 <jfernando020202@gmail.com>
2024-08-09 00:05:44 +1000

    Adjust shape movement

--- TetrisGame/src/model/Game.java
+++ TetrisGame/src/model/Game.java
@@ -1,12 +1,13 @@
 // Manages the game state, updates and overall control
 package model;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
-    int[][] board=new int[20][11]; //Top left is (0,0), bottom right is (20,11)
-    static int[] spawn_location={3,5}; //Top middle of board
+    int width=11;
+    int height=20;
+    int[][] board=new int[width][height]; //Top left is (0,0), bottom right is (20,11)
+    static int[] spawn_location={5,3}; //Top middle of board
     TetrisShape active_shape;
 
     public Game(){
@@ -16,23 +17,39 @@ public class Game {
     }
 
     private void display(){
-        for (int[] row : board) {
-            System.out.println(Arrays.toString(row));
+        for(int j = 0; j < height; j++) {
+            for(int i = 0; i < width; i++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
         }
     }
     public void play(){
+        int[][] move;
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter input: ");
         String input = scanner.nextLine();  // Read user input
         clearActiveShape();
         // Process user input
         if(input.equals("up")){
-            active_shape.rotate();
+            move=active_shape.rotate();
+            if(validMove(move)){
+                active_shape.moveShape(move);
+            }
         }
         else if(input.equals("left") || input.equals("right")){
-            active_shape.translate(input);
+            move=active_shape.translate(input);
+            if(validMove(move)){
+                active_shape.moveShape(move);
+            }
         }
-        active_shape.translate("down");
+        move=active_shape.translate("down");
+        if(validMove(move)){
+            active_shape.moveShape(move);
+        }
+        else{
+            System.out.println("Landed!");
+        }
         update();
         display();
     }
@@ -55,5 +72,22 @@ public class Game {
             int boardY = spawn_location[1] + active_shape.getCoordinates()[i][1];
             board[boardX][boardY]=active_shape.getId();
         }
+    }
+
+    private boolean validMove(int[][] coordinates){
+        int x;
+        int y;
+        for (int[] coordinate : coordinates) {
+            x = coordinate[0];
+            y = coordinate[1];
+            System.out.println(spawn_location[0] + x);
+            System.out.println(spawn_location[1] + y);
+            if (spawn_location[0] + x > width || spawn_location[0] + x < 0 ||
+                    spawn_location[1] + y > height || spawn_location[1] + y < 0 ||
+                    board[spawn_location[0] + x][spawn_location[1] + y] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }

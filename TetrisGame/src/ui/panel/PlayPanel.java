38da46bebe46947af4449e24bb2903d4c6db2ec4
Stefan Barosan <stefanmb@hotmail.com>
2024-09-12 15:51:51 +1000

    minor bug fixes

--- TetrisGame/src/ui/panel/PlayPanel.java
+++ TetrisGame/src/ui/panel/PlayPanel.java
@@ -1,6 +1,8 @@
 // Panel specific to gameplay: shows controls, active game display, current score (fetched from model.Score), etc.
 package ui.panel;
 
+// Play panel holds fieldPane and Score logic updated by game
 public class PlayPanel {
+    // grid of left and right
 
 }

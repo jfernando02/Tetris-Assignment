7706558a451861136ad5b6ab8de5aecb28a78a88
Stefan Barosan <stefanmb@hotmail.com>
2024-09-12 13:27:24 +1000

    Fixed reset config  bug not refreshing board

--- TetrisGame/src/ui/MainFrame.java
+++ TetrisGame/src/ui/MainFrame.java
@@ -346,5 +346,7 @@ public class MainFrame extends JFrame {
     public void resetConfigData() {
         ConfigManager.resetConfigData();
         this.configData = ConfigManager.getConfigData();
+        // Refreshes the board dimensions after a reset
+        refreshBoard();
     }
 }
